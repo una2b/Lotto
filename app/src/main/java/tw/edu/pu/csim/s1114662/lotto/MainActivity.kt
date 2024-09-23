d5ebc7af9a953904ca9858447e74febefec24b96
una2b <syunna2004625@gmail.com>
2024-09-23 15:59:51 +0800

    第二次 Commit

--- app/src/main/java/tw/edu/pu/csim/s1114662/lotto/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/s1114662/lotto/MainActivity.kt
@@ -1,6 +1,7 @@
 package tw.edu.pu.csim.s1114662.lotto
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +16,10 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
         }
+
+        var txv: TextView = findViewById(R.id.txv)
+        txv.text = "0"
     }
 }
